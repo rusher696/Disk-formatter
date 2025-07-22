98d81c87be0187e003f1331467ea9ee341143f18
rusher696 <146143667+rusher696@users.noreply.github.com>
2025-07-22 17:55:11 +1000

    Added better formatting
    I have now added formats like format:type or fill:filler:fillsize via format_type= instead of 2 ignored args.

--- diskformatter.py
+++ diskformatter.py
@@ -1,8 +1,14 @@
 import os, subprocess, json
-def killdisk(drive, fs="ntfs quick", type="format", fillmb=1024):
+def killdisk(drive, format_type="format:ntfs quick"):
     if not os.access(drive, os.W_OK) or not os.access(drive, os.R_OK) or not os.access(drive, os.X_OK): # Check disk permissions
         print("Please grant full permissions first before running.")
         return
+    def helper(type):
+        parts = type.strip(":").split(":") # Get parts
+        arg1, arg2, arg3 = (parts[0], parts[1], parts[2]) if type.count(":") == 2 else (parts[0], parts[1], None)
+        return (arg1, arg2, arg3)    
+    type, filler, fillmb = helper(format_type)    
+    if type == "format": fs = filler
     powershellcmd = ["powershell", "-Command", f"Get-Partition -DriveLetter '{drive[0]}' | Get-Disk | Select-Object Number | ConvertTo-Json"] # cmdline args for powershell
     result = subprocess.run(powershellcmd, capture_output=True, text=True)
     data = json.loads(result.stdout)
@@ -31,7 +37,6 @@ def killdisk(drive, fs="ntfs quick", type="format", fillmb=1024):
         os.remove(f"disk{disknum}.txt")
     elif type.lower().strip().startswith("fill"):
         print(f"Found disk {disknum} for drive {drive}")
-        filler = type.split()[1] # Get the second word (e.g. "hello world".split()[0] = "world")
         with open(rf"\\.\PhysicalDrive{disknum}", "wb") as disk:
             filler = filler.encode("utf-8")            
             for _ in range(int(fillmb)):
